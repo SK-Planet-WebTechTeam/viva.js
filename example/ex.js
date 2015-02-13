632267089cb8d0e761f5a80177113c718b0cf5d4
Jungmin Yoon <cheeki101@gmail.com>
2015-02-13 17:02:11 +0900

    update ex.js

--- example/ex.js
+++ example/ex.js
@@ -3,33 +3,36 @@ world.setRenderer( viva.renderer.canvas( "testdiv" ) );
 
 world.apply( viva.behavior.ConstantAcceleration( 0, 9.8 ) );
 world.apply( viva.behavior.BoundaryCollision( {
-	x: 0,
-	y: 0,
-	width: world.renderer.width,
-	height: world.renderer.height,
-	cor: 1
+    x: 0,
+    y: 0,
+    width: world.renderer.width,
+    height: world.renderer.height,
+    cor: 1
 }));
 world.apply( viva.behavior.Collision() );
 
 var body,
-	size = 50;
+    size = 50;
 
-var colors = ["red", "blue", "black", "magenta", "skyblue", "pink", "orange", "purple", "green", "yellow"];
+var colors = ["red", "blue", "gray", "magenta", "skyblue", "pink", "orange", "purple", "green", "yellow"];
 for ( var i = 0; i < 10; i++ ) {
-	body = viva.body.create( "circle", {
-		x: (size + 10) * (i % 10) + 10,
-		y: (size + 10) * Math.floor(i / 10) + 100,
-		radius: size /2,
-		mass: 1,
-		color: "rgb(255," + (i * 20) + ", " + (i * 20) + ")",
-		color: colors[i%10],
-		cor: 0.6,
-		// cof: 0.1,
-		angle: 45 * Math.PI/180 * i
-	});
+    body = viva.body.create( "circle", {
+        x: (size + 20) * (i % 10) + 50,
+        y: (size + 10) * Math.floor(i / 10) + 100,
+        radius: size /2,
+        // width:size,
+        // height:size,
+        mass: 1,
+        color: "rgb(255," + (i * 20) + ", " + (i * 20) + ")",
+        color: colors[i%10],
+        cor: 0.8,
+        // cof: 0.1,
+        angle: 360* Math.random() * Math.PI/180
+    });
 
-	world.add( body );
-	body.applyForce( viva.vector.create( 10 * Math.random() + 30, - (10 * Math.random() + 20) ));
+    world.add( body );
+    body.applyForce( viva.vector.create( 10 * Math.random() + 30, - (10 * Math.random() + 20) ));
 }
 
 setTimeout ( world.start(), 1000);
+
